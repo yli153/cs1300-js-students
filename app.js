7e505adf42d608fe7b9dbd73d611eed918cacccd
yli153 <60232506+yli153@users.noreply.github.com>
2020-10-29 21:14:23 -0400

    completed

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var corsApiUrl = "https://cors-anywhere.herokuapp.com/";
 // TODO: REPLACE YOUR TOKEN
-var apiToken = "?token=YOUR_TOKEN_HERE";
+var apiToken = "?token=ZtimvewWj3iwX0n9AlgxreT9ubCLLTEJeVT_lhOl8Es";
 
 // CORS stands for "cross origin resource sharing" -- you'll be making http requests in order
 // DON'T CHANGE THIS: fetches the data from the API endpoint
@@ -20,12 +20,44 @@ const corsPromise = () =>
     resolve(request);
   });
 
+let isToggle = false;
 // THIS IS SOME SAMPLE CODE FOR HOW TO USE PROMISES -- feel free to adapt this into a function!
-corsPromise().then(
-  (request) =>
-    (request.onload = request.onerror = function () {
-      // TODO: ADD FUNCTION, ETC. FOR WHATEVER YOU WANT TO DO ONCE THE DATA IS RECEIVED
-    })
-);
+const runRequest = () => {
+  isToggle = !isToggle;
+  if (isToggle) {
+    corsPromise().then(
+      (request) =>
+        (request.onload = request.onerror = function () {
+          const plantData = JSON.parse(request.response).data;
+          updateGrid(plantData);
+        })
+    );
+  } else {
+    document.getElementById('plant-grid').innerHTML = "";
+  }
+}
 
 //// TODO: ADD WHATEVER FUN CONTENT YOU WANT ////
+const updateGrid = (plantData) => {
+  plantData.forEach(plant => {
+    const profile = createPlantProfile(plant);
+    document.getElementById('plant-grid').appendChild(profile);
+  })
+}
+
+const createPlantProfile = (plant) => {
+  const profile = document.createElement('div');
+  const name = document.createElement('h3');
+  name.innerHTML = "Common Name: " + plant.common_name;
+
+  const year = document.createElement('h4');
+  year.innerHTML = "Year: " + plant.year;
+
+  const image = document.createElement('img');
+  image.src = plant.image_url;
+
+  profile.appendChild(name);
+  profile.appendChild(year);
+  profile.appendChild(image);
+  return profile;
+}
